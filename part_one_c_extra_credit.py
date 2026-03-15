cbaa9fee14cf8f7435a82c323962cfbe221d93af
kleitherer <kaitlynleitherer@gmail.com>
2026-03-14 20:33:00 -0700

    got rid of files that were outside the folder

--- part_one_c_extra_credit.py
+++
@@ -1,9 +0,0 @@
-"""
-Motion artifact added...
-"""
-from scipy.signal import find_peaks
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.stats as stats
-
-ppg_waveform = np.loadtxt('HW5.1C.PPG.txt')
